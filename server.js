62dcec8bd7c5fd8ff988c36f2bb3c244bcabcb7e
NewChoBo <whwjdrb1009@gmail.com>
2023-02-24 21:03:17 +0900

    기본 설정중... cors 설치만 끝낸 상태

--- server.js
+++ server.js
@@ -6,13 +6,29 @@ app.get('/', (req, res) => {
     res.send('Hello World!!')
 });
 
-app.get('/dog', (req, res) => {
-    //res.send({'sound': 'bark'})
-    res.json({'cat' : '야옹'})
-});
+app.get('/user/:name', (req, res) => {
+    // const p = req.params;
+    // p.name으로만 값을 꺼내올 수 있었음. 이를 아래 코드처럼 단축할 수 있음
 
-app.get('/cat', (req, res) => {
-    res.send('meao')
+    const { name } = req.params;
+
+    console.log('이름 : ' + name);
+
+    switch(name){
+        case 'dog':
+            console.log('hello');
+            res.json({'sound' : '멍멍'});
+            break;
+        case 'cat':
+            res.json({'sound' : '야옹'});
+            return;
+        case 'pig':
+            res.json({'sound' : '꿀꿀'});
+            return;
+        default:
+            // console.log('끝');
+    }
+    res.json(name)
 });
 
 app.listen(port, () => {
