50044978d8430dae967b2eb2a4eff9fe13e1535d
NewChoBo <whwjdrb1009@gmail.com>
2023-02-26 00:29:30 +0900

    마리아DB 설치만 함

--- server.js
+++ server.js
@@ -6,32 +6,26 @@ const port = 3000
 app.use(cors())     //앱에서 cors 사용. 인자에 넣어서 조건 만들 수 있다.
 
 app.get('/', (req, res) => {
-    res.send('Hello World!!!!!!!')
+    res.send('')
 });
 
-app.get('/sound/:name', (req, res) => {
-    // const p = req.params;
-    // p.name으로만 값을 꺼내올 수 있었음. 이를 아래 코드처럼 단축할 수 있음
+app.get('/portfolios', (req, res) => {
+    // const { id } = req.params;    
+    res.json(
+        [
+        {'userID':'aaa', 'portfolioID':'001', 'thum':'https://image.utoimage.com/preview/cp872722/2022/12/202212008462_500.jpg'}, 
+        {'userID':'bbb', 'portfolioID':'002', 'thum':'https://i.pinimg.com/236x/44/af/03/44af0390dab474bbb62a266c0232d31a.jpg'},
+        {'userID':'ccc', 'portfolioID':'001', 'thum':'https://m.jungle.co.kr/image/89a947214133b7e4fc201025'}, 
+        {'userID':'dddd', 'portfolioID':'002', 'thum':'https://i.pinimg.com/564x/fc/00/2c/fc002c18734bf9216477ca5359ca8d69.jpg'},
+        {'userID':'ee', 'portfolioID':'001', 'thum':'https://t1.daumcdn.net/cfile/tistory/117A8433516D5BF70E'}, 
+        {'userID':'fff', 'portfolioID':'002', 'thum':'https://mblogthumb-phinf.pstatic.net/MjAxNjExMjVfMTM4/MDAxNDgwMDg0MDQ2NTU0.K5BVEWt2gK2tHm1ibBIohf9y1MKPpZCDPMah5DwFcMUg.i0-Ikbh0kdknu55sejzN02fPQwp2JCYXgvOpgCSiPtMg.JPEG.knicjin/021.jpg?type=w800'},
+        {'userID':'ggg', 'portfolioID':'001', 'thum':'https://t1.daumcdn.net/cfile/tistory/99B634495AA0EC0310'}, 
+        {'userID':'hhh', 'portfolioID':'002', 'thum':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL_BtRrE_V_kluwvyYEcZ0tyzEX7AZbvA7_g&usqp=CAU'}
+    ]);
+});
 
-    const { name } = req.params;
-
-    console.log('이름 : ' + name);
-
-    switch(name){
-        case 'dog':
-            console.log('hello');
-            res.json({'sound' : '멍멍'});
-            break;
-        case 'cat':
-            res.json({'sound' : '야옹'});
-            return;
-        case 'pig':
-            res.json({'sound' : '꿀꿀'});
-            return;
-        default:
-            // console.log('끝');
-    }
-    // res.json(name)
+app.get('/*', (req, res) => {
+    res.redirect('/');
 });
 
 app.listen(port, () => {
